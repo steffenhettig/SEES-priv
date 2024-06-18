2c507bfed25cae830c18cbbdb625e86b0855f76e
steffenhettig <145577050+steffenhettig@users.noreply.github.com>
2024-06-18 17:18:28 +0200

    Gesicherter Zwischenstand vor großer Änderung

---
+++ Driving.c
@@ -0,0 +1,183 @@
+/***************************************************************************************************
+  (c) NewTec GmbH 2024   -   www.newtec.de
+***************************************************************************************************/
+/**
+ * @file       Driving.c
+ *
+ * Module description comes here.
+ */
+ /**************************************************************************************************/
+
+ /* INCLUDES ***************************************************************************************/
+#include <stdio.h>
+#include "Driving.h"
+
+// #include "app/StateHandler.h"
+#include "service/DriveControl.h"
+//#include "DriveControl.h"
+//#include "TickTimer.h"
+
+
+ /* CONSTANTS **************************************************************************************/
+
+ /* MACROS *****************************************************************************************/
+
+ /* TYPES ******************************************************************************************/
+
+ /* PROTOTYPES *************************************************************************************/
+/** This function is used to check if a sensor is over a line
+* @param[in] value sensor values
+* @param[in] *parameterSet Passes the struct with the set parameters
+* @return Bool true if sucessfull
+*/
+static Bool sensorOverLine(UInt16 value);
+
+/** This function is used to check if a sensor is not over a line 
+* @param[in] value sensor values
+* @param[in] *parameterSet Passes the struct with the set parameters
+* @return Bool true if sucessfull
+*/
+static Bool sensorNoLine(UInt16 value);
+ /* VARIABLES **************************************************************************************/
+
+static void regulateSpeed(Int32 error, UInt16 * leftSpeed, UInt16 * rightSpeed);
+static UInt32 calculatePosition(const LineSensorValues *sensorValues);
+static UInt32 glastPostion = 0U;
+static UInt32 gLastError = 0U;
+const float gK_p = 0.25;
+const UInt32 gK_d = 6;
+
+ /* EXTERNAL FUNCTIONS *****************************************************************************/
+void Driving_stopDriving(void)
+{
+    DriveControl_drive(DRIVE_CONTROL_MOTOR_LEFT, 0, DRIVE_CONTROL_FORWARD);
+    DriveControl_drive(DRIVE_CONTROL_MOTOR_RIGHT, 0, DRIVE_CONTROL_FORWARD);
+}
+void Driving_driveForward(void)
+{
+    DriveControl_drive(DRIVE_CONTROL_MOTOR_LEFT, MAX_MOTOR_SPEED, DRIVE_CONTROL_FORWARD);
+    DriveControl_drive(DRIVE_CONTROL_MOTOR_RIGHT, MAX_MOTOR_SPEED, DRIVE_CONTROL_FORWARD);  
+}
+
+// Implementation of the RunRace_Process method
+void Driving_followLine(LineSensorValues * SensorValues) 
+{
+    UInt32 position = calculatePosition(SensorValues);
+    //UInt32 error = position - CENTER_OF_LINE_POSITION;
+    UInt32 error = position - ((LINESENSOR_COUNT - 1) * SENSOR_WEIGHT_SCALE / 2);
+
+    UInt16 leftSpeed;
+    UInt16 rightSpeed;
+
+    regulateSpeed(error, &leftSpeed, &rightSpeed);
+
+    DriveControl_drive(DRIVE_CONTROL_MOTOR_LEFT, leftSpeed, DRIVE_CONTROL_FORWARD);
+    DriveControl_drive(DRIVE_CONTROL_MOTOR_RIGHT, rightSpeed, DRIVE_CONTROL_FORWARD);
+}
+
+
+/* INTERNAL FUNCTIONS *****************************************************************************/
+
+static void regulateSpeed(Int32 error, UInt16 * leftSpeed, UInt16 * rightSpeed)
+{
+    /* PID controller */
+    Int32 proportional = (Int32)((error * PROP_NUM) / PROP_DENOM);
+    Int32 derivative   = (error - gLastError) * DERIV_NUM / DERIV_DENOM;
+    Int32 integral     = 0;  /* not needed */
+    Int32 speedDifference = proportional + derivative + integral;
+
+    Int32 left = MAX_MOTOR_SPEED + speedDifference;
+    Int32 right = MAX_MOTOR_SPEED - speedDifference;
+
+    if (left < 0)
+    {
+        left = 0;
+    }
+    if (right < 0)
+    {
+        right = 0;
+    }
+
+    if (left > MAX_MOTOR_SPEED)
+    {
+        left = MAX_MOTOR_SPEED;
+    }
+
+    if (right > MAX_MOTOR_SPEED)
+    {
+        right = MAX_MOTOR_SPEED;
+    }
+
+    *leftSpeed = left;
+    *rightSpeed = right;
+
+    gLastError = error;
+}
+
+static UInt32 calculatePosition(const LineSensorValues * sensorValues)
+{
+    UInt32 position = 0u;
+    UInt32 sum = 0u;
+    UInt32 weight = 0u;
+    bool foundLine = false;
+
+    for (UInt8 sensor = 0; sensor < LINESENSOR_COUNT; ++sensor)
+    {
+        UInt32 val = sensorValues->value[sensor];
+
+        // if (CALIB_OVER_LINE(val))
+        // {
+        //     foundLine = true;
+        // }
+        if (sensorOverLine(val))
+        {
+            foundLine = true;
+        }
+
+        position += val * weight;
+        sum += val;
+
+        weight += SENSOR_WEIGHT_SCALE;
+    }
+
+    if (!foundLine)
+    {
+        position = glastPostion;
+    }
+    else
+    {
+        /* build weighted average */
+        position /= sum;
+        glastPostion = position;
+    }
+
+    return position;
+}
+
+static Bool sensorOverLine(UInt16 value)
+{
+    Bool ret;
+    if (value < 500U)
+    {
+        ret = FALSE;
+    }
+    else
+    {
+        ret = TRUE;
+    }
+    return ret;
+}
+
+static Bool sensorNoLine(UInt16 value)
+{
+    Bool ret;
+    if (value > 250U)
+    {
+        ret = FALSE;
+    }
+    else
+    {
+        ret = TRUE;
+    }
+    return ret;
+}
